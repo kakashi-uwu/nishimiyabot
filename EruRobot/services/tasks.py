d59a06d2330781c67ad763ccf7566a0288c116aa
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-02-20 19:00:52 +0530

    Update tasks.py

--- EruRobot/services/tasks.py
+++ EruRobot/services/tasks.py
@@ -2,8 +2,8 @@ from asyncio import Lock, create_task
 from time import time
 from pyrogram import filters
 from pyrogram.types import Message
-from SiestaRobot import BOT_ID, BOT_USERNAME, DEV_USERS
-from SiestaRobot.services.sections import bold, section, w
+from EruRobot import BOT_ID, BOT_USERNAME, DEV_USERS
+from EruRobot.services.sections import bold, section, w
 tasks = {}
 TASKS_LOCK = Lock()
 arrow = lambda x: (x.text if x else "") + "\n`→`"
