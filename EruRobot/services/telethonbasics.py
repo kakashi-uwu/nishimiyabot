9e5cfb6501789f9678c74aae4c65fb2e0772b0b2
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-02-20 19:01:45 +0530

    Update telethonbasics.py

--- EruRobot/services/telethonbasics.py
+++ EruRobot/services/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from SiestaRobot.utils.pluginhelper import runcmd
+from EruRobot.utils.pluginhelper import runcmd
 
 
 async def convert_to_image(event, borg):
