62a68b9fbb580025b16fe27943c1061691d8f498
Kokushibo-Kizuki-1 <98401272+Kokushibo-kizuki-1@users.noreply.github.com>
2022-02-20 18:52:54 +0530

    Update functions.py

--- EruRobot/utils/functions.py
+++ EruRobot/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from SiestaRobot import aiohttpsession as aiosession
-from SiestaRobot.ex_plugins.dbfunctions import start_restart_stage
-from SiestaRobot.utils.http import get, post
+from EruRobot import aiohttpsession as aiosession
+from EruRobot.ex_plugins.dbfunctions import start_restart_stage
+from EruRobot.utils.http import get, post
 
 
 async def restart(m: Message):
