2edea372b703ac824c1bb34a4be95389bd3f13e4
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-02-21 09:13:51 +0530

    Update logomaker.py

--- EruRobot/modules/logomaker.py
+++ EruRobot/modules/logomaker.py
@@ -69,7 +69,7 @@ async def logo_gen(event):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./SiestaRobot/resources/fonts/*")
+        fpath_ = glob.glob("./EruRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
@@ -80,7 +80,7 @@ async def logo_gen(event):
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./SiestaRobot/resources/fonts/*")
+        fpath_ = glob.glob("./EruRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 120
@@ -114,7 +114,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
+            caption="here is your logo! made by [Eru Robot](https://t.me/eruxrobot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -122,7 +122,7 @@ async def logo_gen(event):
     if os.path.exists(bg_):
         os.remove(bg_) 
     if os.path.exists(font_):
-        if not font_.startswith("./SiestaRobot/resources/fonts"):
+        if not font_.startswith("./EruRobot/resources/fonts"):
             os.remove(font_)
 
 
@@ -152,7 +152,7 @@ async def logo_(event):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./SiestaRobot/resources/fonts/*")
+        fpath_ = glob.glob("./EruRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
@@ -163,7 +163,7 @@ async def logo_(event):
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./SiestaRobot/resources/fonts/*")
+        fpath_ = glob.glob("./EruRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 105
@@ -197,7 +197,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
+            caption="here is your wlogo! made by  [Eru Robot](https://t.me/eruxrobot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -205,7 +205,7 @@ async def logo_(event):
     if os.path.exists(bg_):
         os.remove(bg_) 
     if os.path.exists(font_):
-        if not font_.startswith("./SiestaRobot/resources/fonts"):
+        if not font_.startswith("./EruRobot/resources/fonts"):
             os.remove(font_)
 
 
