75ded4fdb51538b99ed562265a82b4b7d542112a
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-02-22 14:24:46 +0530

    Update __main__.py

--- EruRobot/__main__.py
+++ EruRobot/__main__.py
@@ -911,7 +911,7 @@ def main():
     updater.idle()
 
 
-if name == "main":
+if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
     pbot.start()
