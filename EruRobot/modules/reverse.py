f3f6fc5c893bc73c1d67cd824183a259de261eb1
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-02-28 23:56:50 +0530

    Update reverse.py

--- EruRobot/modules/reverse.py
+++ EruRobot/modules/reverse.py
@@ -182,7 +182,7 @@ def scam(imgspage, lim):
 
 __help__ = """
 ➢ `/reverse`*:* reply to any image or sticker to get results"""
-__mod_name__ = "「REVERSE」"
+__mod_name__ = "Rᴇᴠᴇʀsᴇ"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
     ["reverse", "grs" , "p"], reverse, pass_args=True, admin_ok=True
