f66e86d1039271bb0b43434a627bfec16e5e1a05
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-02-28 16:58:07 +0530

    Update apakah.py

--- EruRobot/modules/apakah.py
+++ EruRobot/modules/apakah.py
@@ -2,24 +2,28 @@ import random
 from EruRobot.events import register
 from EruRobot import telethn
 
-APAKAH_STRING = ["Iya", 
-                 "Tidak", 
-                 "Mungkin", 
-                 "Mungkin Tidak", 
-                 "Bisa jadi", 
-                 "Mungkin Tidak",
-                 "Tidak Mungkin",
-                 "YNTKTS",
-                 "Pala bapak kau pecah",
-                 "Apa iya?",
-                 "Tanya aja sama mamak kau tu pler"
+APAKAH_STRING = ["chala ja bsdk", 
+                 "No idea", 
+                 "Sasta hai", 
+                 "India hai, chalta hai", 
+                 "Arey you Pointing me out", 
+                 "That's nonsense",
+                 "Impossible",
+                 "Okay go on",
+                 "Ask me somthing else",
+                 "Really ?",
+                 "baby-kun is my boyfriend,god, owner and love"
                  ]
 
 
-@register(pattern="^/apakah ?(.*)")
+@register(pattern="^/what ?(.*)")
 async def apakah(event):
     quew = event.pattern_match.group(1)
     if not quew:
-        await event.reply('Berikan saya pertanyaan 😐')
+        await event.reply('gimme some questions')
         return
     await event.reply(random.choice(APAKAH_STRING))
+
+__help__ = """
+➢ `/what`*:* <argument> you'll get some random.....or maybe annoying answers"""
+__mod_name__ = "「❣️WHAT❣️」"
