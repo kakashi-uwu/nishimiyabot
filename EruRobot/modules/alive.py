8100c71d9163b64963e09281be986901364d8759
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-03-11 12:23:32 +0530

    Update alive.py

--- EruRobot/modules/alive.py
+++ EruRobot/modules/alive.py
@@ -11,13 +11,13 @@ from EruRobot import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/1f55761762cf30a02cb22.mp4"
 
-@register(pattern=("/alive"))
+@register(pattern=("/awake"))
 async def awake(event):
   TEXT = f"**Kᴏɴ'ɴɪᴄʜɪᴡᴀ [{event.sender.first_name} san](tg://user?id={event.sender.id}), I'ᴍ ᴇʀᴜ.** \n\n"
   TEXT += "**╔═══──────═══╗** \n\n"
   TEXT += f"┣[× **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"┣[× **Tᴇʟᴇᴛʜᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"┣[× **ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += "**╚═══──────═══╝"
+  TEXT += "╚═══──────═══╝"
   BUTTON = [[Button.url("Hᴇʟᴘ", "https://t.me/eruxrobot?start=help"), Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/eruxsupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
