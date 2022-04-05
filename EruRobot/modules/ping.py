021c13c8feb0d593d1ea732101c73704144ea7fa
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-04-05 17:19:22 +0530

    Update ping.py

--- EruRobot/modules/ping.py
+++ EruRobot/modules/ping.py
@@ -79,7 +79,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "<b>["https://telegra.ph/file/815bbf316d0f735f780c3.jpg"]PONG</b> ✨\n"
+        "<b>("https://telegra.ph/file/815bbf316d0f735f780c3.jpg"), PONG</b> ✨\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
