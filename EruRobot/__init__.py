f5246859e608757e31e0ed110375053f4dd14472
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-04-12 15:50:59 +0530

    Update __init__.py

--- EruRobot/__init__.py
+++ EruRobot/__init__.py
@@ -11,6 +11,7 @@ import telegram.ext as tg
 from inspect import getfullargspec
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
+from redis import StrictRedis
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from telethon.sessions import MemorySession
@@ -98,6 +99,7 @@ if ENV:
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    REDIS_URL = os.environ.get("REDIS_URL", None)  # REDIS URL (From:- Heraku & Redis)
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
@@ -177,6 +179,7 @@ else:
     ERROR_LOGS = Config.ERROR_LOGS
     DB_URL = Config.SQLALCHEMY_DATABASE_URI
     MONGO_DB_URI = Config.MONGO_DB_URI
+    REDIS_URL = Config.REDIS_URL
     ARQ_API = Config.ARQ_API_KEY
     ARQ_API_URL = Config.ARQ_API_URL
     DONATION_LINK = Config.DONATION_LINK
